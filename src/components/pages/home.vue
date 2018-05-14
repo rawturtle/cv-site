b9a868250b6871bae1c38bd1c518a3df25451fd0
rawturtle <c.rawstorn@gmail.com>
2018-05-14 15:44:59 +1200

    restructuring home page

--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -1,185 +1,212 @@
 <template>
-    <div class="tile">
-
-      <div class="tile is-parent is-4">
-        <div class="tile is-child contact">
-          <div class="card">
-            <header class="card-header">
-              <p class="card-header-title">
-                Contact
-              </p>
-            </header>
-            <div class="card-image">
-              <figure class="image">
-                <img src="../../assets/images/myface.jpg" alt="My face">
-              </figure>
+  <section>
+    <div class="columns">
+      <div class="column">
+        <div class="tile">
+          <div class="tile is-parent is-4">
+            <div class="tile is-child contact">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">
+                    Contact
+                  </p>
+                </header>
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="../../assets/images/myface.jpg" alt="My face">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <p class=""><i class="fas fa-user"></i> Charlie Rawstorn</p>
+                  <i class="fas fa-envelope"></i> <a href="mailto:c.rawstorn@gmail.com"> c.rawstorn@gmail.com</a>
+                  <p><i class="fas fa-phone"></i> 027 3466 031</p>
+                  <i class="fab fa-github"></i><a href="https://github.com/rawturtle"> rawturtle</a>
+                </div>
+              </div>
             </div>
-            <div class="card-content">
-              <p class=""><i class="fas fa-user"></i> Charlie Rawstorn</p>
-              <i class="fas fa-envelope"></i> <a href="mailto:c.rawstorn@gmail.com"> c.rawstorn@gmail.com</a>
-              <p><i class="fas fa-phone"></i> 027 3466 031</p>
-              <i class="fab fa-github"></i><a href="https://github.com/rawturtle"> rawturtle</a>
+          </div>
+
+          <div class="tile is-parent is-3 is-vertical">
+            <div class="tile is-child about">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">
+                    About Me
+                  </p>
+                </header>
+                <div class="card-content">
+                  <div class="content">
+                    <p>I was living in Melbourne teaching students how to SCUBA dive before I decided to leave my
+                      underwater office and head to dry(ish) land in Otago to study.</p>
+
+                    <p>I recently finished a BSc in Computer Science at Otago University. I am looking for work in the
+                      Dunedin Tech sector</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="tile is-child interests">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">
+                    Interests
+                  </p>
+                </header>
+                <div class="card-content">
+                  <div class="content">
+                    <p>I spend most of my time obsessing over upcoming swells/weather patterns and the latest tech
+                      trends
+                      on
+                      <a href="https://medium.com">Medium</a> and <a href="https://reddit.com">Reddit</a></p>
+                    <p>My interests include:</p>
+                    <ul>
+                      <li>Surfing</li>
+                      <li>More surfing</li>
+                      <li>Coding</li>
+                      <li>Video Games</li>
+                      <li>Skateboarding</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="tile is-parent is-vertical is-5">
+            <div class="tile is-child work-history">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">
+                    Work History
+                  </p>
+                </header>
+                <div class="content">
+
+                  <div class="card work-card">
+                    <div class="card-content">
+                      <p class="header"><i class="fas fa-building"></i><a href="https://tecto.com/">Tecto Ltd</a></p>
+                      <div class="columns">
+                        <div class="column is-5">
+                          <p>March 2018 to Present</p>
+                        </div>
+                        <div class="column">
+                          <p>Web Developer<br> Using Vue.js to create bespoke web
+                            applications</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card work-card">
+                    <div class="card-content">
+                      <p><i class="fas fa-building"></i><a href="http://www.igtimi.com/">Igtimi Ltd</a></p>
+                      <div class="columns">
+                        <div class="column is-5">Nov 2017 to Dec 2017</div>
+                        <div class="column">
+                          <p>Internship <br> Creating an application to parse config files into a GUI so they can be
+                            reconfigured by the client.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card work-card">
+                    <div class="card-content">
+                      <p><i class="fas fa-building"></i><a href="https://www.otago.ac.nz/">Otago University</a></p>
+                      <div class="columns">
+                        <div class="column is-5">Nov 2017 to Dec 2017</div>
+                        <div class="column">
+                          <p>Tutor<br>Providing tutelage for first year Computer Science students taking Comp160,
+                            Comp150
+                            and Comp101.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card work-card">
+                    <div class="card-content">
+                      <p><i class="fas fa-building"></i><a href="http://www.igtimi.com/">Igtimi Ltd</a></p>
+                      <div class="columns">
+                        <div class="column is-5">Nov 2016 to Dec 2016</div>
+                        <div class="column">
+                          <p>Internship<br>Updating existing webpages to be relevant and mobile friendly, creating new
+                            product pages and contact forms. </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
+    <!-- Education row -->
+    <div class="columns">
+      <div class="column">
+        <div class="tile">
+          <div class="tile education is-parent">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Education
+                </p>
+              </header>
+              <div class="columns">
 
-      <div class="tile is-parent is-3 is-vertical">
-        <div class="tile is-child about">
-          <div class="card">
-            <header class="card-header">
-              <p class="card-header-title">
-                About Me
-              </p>
-            </header>
-            <div class="card-content">
-              <div class="content">
-                <p>I was living in Melbourne teaching students how to SCUBA dive before I decided to leave my
-                  underwater office and head to dry(ish) land in Otago to study.</p>
-
-                <p>I recently finished a BSc in Computer Science at Otago University. I am looking for work in the Dunedin Tech sector</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="tile is-child interests">
-          <div class="card">
-            <header class="card-header">
-              <p class="card-header-title">
-                Interests
-              </p>
-            </header>
-            <div class="card-content">
-              <div class="content">
-                <p>I spend most of my time obsessing over upcoming swells/weather patterns and the latest tech trends on
-                  <a href="https://medium.com">Medium</a> and <a href="https://reddit.com">Reddit</a></p>
-                <p>My interests include:</p>
-                <ul>
-                  <li>Surfing</li>
-                  <li>More surfing</li>
-                  <li>Coding</li>
-                  <li>Video Games</li>
-                  <li>Skateboarding</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="tile is-parent is-vertical is-5">
-        <div class="tile is-child work-history">
-          <div class="card">
-            <header class="card-header">
-              <p class="card-header-title">
-                Work History
-              </p>
-            </header>
-            <div class="content">
-
-              <div class="card">
-                <div class="card-content">
-                  <p class="header"><i class="fas fa-building"></i><a href="https://tecto.com/">Tecto Ltd</a></p>
-                  <div class="columns">
-                    <div class="column is-5">
-                      <p><i class="fas fa-calendar-alt"></i>March 2018 to Present</p>
-                    </div>
-                    <div class="column">
-                      <p><i class="fas fa-tasks"></i>Web Developer <br> Using Vue.js to create bespoke web applications</p>
+                <div class="column">
+                  <div class="card education-card">
+                    <div class="card-content">
+                      <p><i class="fas fa-calendar-alt"></i> <strong>2015 - 2017</strong></p>
+                      <p><i class="fas fa-university"></i> University of Otago</p>
+                      <p><i class="fas fa-graduation-cap"></i> BSc majoring in Computer Science</p>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div class="card">
-                <div class="card-content">
-                  <p><i class="fas fa-building"></i><a href="http://www.igtimi.com/">Igtimi Ltd</a></p>
-                  <div class="columns">
-                    <div class="column is-5">Nov 2017 to Dec 2017</div>
-                    <div class="column">
-                      <p>Internship <br> Creating an application to parse config files into a GUI so they can be reconfigured by the client.</p>
+                <div class="column">
+                  <div class="card education-card">
+                    <div class="card-content">
+                      <p><i class="fas fa-calendar-alt"></i> <strong>2013</strong></p>
+                      <p><i class="fas fa-university"></i> Dive Otago</p>
+                      <p><i class="fas fa-graduation-cap"></i> Diploma in Professional SCUBA Instruction</p>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div class="card">
-                <div class="card-content">
-                  <p><i class="fas fa-building"></i><a href="https://www.otago.ac.nz/">Otago University</a></p>
-                  <div class="columns">
-                    <div class="column is-5">Nov 2017 to Dec 2017</div>
-                    <div class="column">
-                      <p>Tutor<br>Providing tutelage for first year Computer Science students taking Comp160, Comp150 and Comp101.</p>
+                <div class="column">
+                  <div class="card education-card">
+                    <div class="card-content">
+                      <p><i class="fas fa-calendar-alt"></i> <strong>2012</strong></p>
+                      <p><i class="fas fa-university"></i> Outward Bound</p>
+                      <p><i class="fas fa-graduation-cap"></i> Certificate of Completion</p>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div class="card">
-                <div class="card-content">
-                  <p><i class="fas fa-building"></i><a href="http://www.igtimi.com/">Igtimi Ltd</a></p>
-                  <div class="columns">
-                    <div class="column is-5">Nov 2016 to Dec 2016</div>
-                    <div class="column">
-                      <p>Internship<br>Updating existing webpages to be relevant and mobile friendly, creating new product pages and contact forms. </p>
+                <div class="column">
+                  <div class="card education-card">
+                    <div class="card-content">
+                      <p><i class="fas fa-calendar-alt"></i> <strong>2009</strong></p>
+                      <p><i class="fas fa-university"></i> CPIT</p>
+                      <p><i class="fas fa-graduation-cap"></i> Certificate of Adventure Recreation</p>
                     </div>
                   </div>
                 </div>
               </div>
 
             </div>
+            <!-- End Education row -->
+
           </div>
         </div>
       </div>
-
     </div>
 
-
-
-  <!--
-  <div class="tile is-child education">
-          <div class="card">
-            <header class="card-header">
-              <p class="card-header-title">
-                Education
-              </p>
-            </header>
-            <div class="content">
-
-              <div class="card">
-                <div class="card-content">
-                  <p><i class="fas fa-calendar-alt"></i> <strong>2015 - 2017</strong></p>
-                  <p><i class="fas fa-university"></i> University of Otago</p>
-                  <p><i class="fas fa-graduation-cap"></i> BSc majoring in Computer Science</p>
-                </div>
-              </div>
-
-              <div class="card">
-                <div class="card-content">
-                  <p><i class="fas fa-calendar-alt"></i> <strong>2013</strong></p>
-                  <p><i class="fas fa-university"></i> Dive Otago</p>
-                  <p><i class="fas fa-graduation-cap"></i> Diploma in Professional SCUBA Instruction</p>
-                </div>
-              </div>
-
-              <div class="card">
-                <div class="card-content">
-                  <p><i class="fas fa-calendar-alt"></i> <strong>2012</strong></p>
-                  <p><i class="fas fa-university"></i> Outward Bound</p>
-                  <p><i class="fas fa-graduation-cap"></i> Certificate of Completion</p>
-                </div>
-              </div>
-              <div class="card">
-              <div class="card-content">
-              <p><i class="fas fa-calendar-alt"></i>  <strong>2009</strong></p>
-              <p><i class="fas fa-university"></i>  CPIT</p>
-              <p><i class="fas fa-graduation-cap"></i>  Certificate of Adventure Recreation</p>
-              </div>
-              </div>
-            </div>
-          </div>
-        </div>
-  -->
+  </section>
 </template>
 
 <script>
@@ -189,23 +216,45 @@
 </script>
 
 <style scoped>
+  .work-card {
+    border: 1px solid rgba(10,10,10,.1);
+    border-bottom: none;
+    box-shadow: none;
+  }
+  .education-card {
+    border: none;
+    box-shadow: none;
+    min-height: 100%;
+
+  }
+
+  /*.column {*/
+    /*background-color: #fff;*/
+  /*}*/
+
   i {
     width: 20px;
   }
 
   ul {
-    margin-top: 0 !important;
+    margin-top: 0;
   }
 
-  .card-header-title {
-    /*background-color: #e7e7e7;*/
+  .card-header {
+    background-color: #e7e7e7 ;
+  }
+  .card {
+    box-sizing: border-box;
   }
 
   .education p {
     margin-bottom: 0;
   }
+
   .card-content {
     padding-top: 10px;
   }
-
+/*
+rgba(10,10,10,.1)
+*/
 </style>
